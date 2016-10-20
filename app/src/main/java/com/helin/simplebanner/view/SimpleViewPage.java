5324a63e795f3da72b806c944dd51cf766981662
helin <991293917@qq.com>
2016-10-20 17:24:52 +0800

    增加自动播放，点击事件

--- app/src/main/java/com/helin/simplebanner/view/SimpleViewPage.java
+++ app/src/main/java/com/helin/simplebanner/view/SimpleViewPage.java
@@ -4,8 +4,10 @@ import android.content.Context;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 
 import com.helin.simplebanner.adapter.SimplePageAdapter;
+import com.helin.simplebanner.listener.OnItemClickListener;
 
 /**
  * Created by helin on 2016/10/19 16:23.
@@ -13,7 +15,11 @@ import com.helin.simplebanner.adapter.SimplePageAdapter;
 public class SimpleViewPage extends ViewPager {
     private OnPageChangeListener mOuterPageChangeListener;
     private SimplePageAdapter mAdapter;
-
+    private OnItemClickListener onItemClickListener;
+    private boolean canLoop = true;
+    private boolean isCanScroll = true;
+    private float oldX = 0, newX = 0;
+    private static final float sens = 5;
     public SimpleViewPage(Context context) {
         super(context);
         init();
@@ -29,9 +35,87 @@ public class SimpleViewPage extends ViewPager {
     }
 
     private void init() {
-        
+        super.addOnPageChangeListener(onPageChangeListener);
     }
-    private boolean canLoop = true;
+
+    private OnPageChangeListener onPageChangeListener = new OnPageChangeListener() {
+        private float mPreviousPosition = -1;
+        @Override
+        public void onPageSelected(int position) {
+            int realPosition = mAdapter.toRealPosition(position);
+            if (mPreviousPosition != realPosition) {
+                mPreviousPosition = realPosition;
+                if (mOuterPageChangeListener != null) {
+                    mOuterPageChangeListener.onPageSelected(realPosition);
+                }
+            }
+        }
+
+        @Override
+        public void onPageScrolled(int position, float positionOffset,
+                                   int positionOffsetPixels) {
+            int realPosition = position;
+            if (mOuterPageChangeListener != null) {
+                if (realPosition != mAdapter.getRealCount() - 1) {
+                    mOuterPageChangeListener.onPageScrolled(realPosition,
+                            positionOffset, positionOffsetPixels);
+                } else {
+                    if (positionOffset > .5) {
+                        mOuterPageChangeListener.onPageScrolled(0, 0, 0);
+                    } else {
+                        mOuterPageChangeListener.onPageScrolled(realPosition,
+                                0, 0);
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+            if (mOuterPageChangeListener != null) {
+                mOuterPageChangeListener.onPageScrollStateChanged(state);
+            }
+        }
+    };
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (isCanScroll) {
+            if (onItemClickListener != null) {
+                switch (ev.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        oldX = ev.getX();
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        newX = ev.getX();
+                        if (Math.abs(oldX - newX) < sens) {
+                            onItemClickListener.onItemClick((getRealItem()));
+                        }
+                        oldX = 0;
+                        newX = 0;
+                        break;
+                }
+            }
+            return super.onTouchEvent(ev);
+        } else
+            return false;
+    }
+
+    public boolean isCanScroll() {
+        return isCanScroll;
+    }
+
+    public void setCanScroll(boolean isCanScroll) {
+        this.isCanScroll = isCanScroll;
+    }
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (isCanScroll)
+            return super.onInterceptTouchEvent(ev);
+        else
+            return false;
+    }
+
     public void setAdapter(PagerAdapter adapter, boolean canLoop) {
         mAdapter = (SimplePageAdapter) adapter;
         mAdapter.setCanLoop(canLoop);
@@ -47,22 +131,43 @@ public class SimpleViewPage extends ViewPager {
 
     public void setCanLoop(boolean canLoop) {
         this.canLoop = canLoop;
-        if (canLoop == false) {
+        if (!canLoop) {
             setCurrentItem(getRealItem(), false);
         }
         if (mAdapter == null) return;
         mAdapter.setCanLoop(canLoop);
         mAdapter.notifyDataSetChanged();
     }
+
+    /**
+     * 获取item数量
+     * @return
+     */
     public int getFristItem() {
         return canLoop ? mAdapter.getRealCount() : 0;
     }
 
+    /**
+     * 获取真实的count
+     * @return
+     */
     public int getRealItem() {
         return mAdapter != null ? mAdapter.toRealPosition(super.getCurrentItem()) : 0;
     }
+
+    /**
+     * 添加页面改变监听
+     * @param listener
+     */
     @Override
     public void addOnPageChangeListener(OnPageChangeListener listener) {
-        super.addOnPageChangeListener(listener);
+        mOuterPageChangeListener = listener;
     }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
+    }
+
+
+
 }
